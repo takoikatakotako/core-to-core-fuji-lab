84b5fecc448eff066c1b533570d3f75b36b7c71c
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2021-08-18 15:49:21 +0900

    header を更新

--- includes/header.js
+++ includes/header.js
@@ -1,18 +1,18 @@
 const header = `
 <header>
-    <h1 id="logo"><a href="index.html"><img src="images/logo.png" alt="SAMPLE COMPANY"></a></h1>
     <!--PC用（901px以上端末）メニュー-->
     <nav id="menubar">
         <ul>
             <li><a href="index.html">Home</a></li>
-            <li><a href="project-schema.html">Project Scheme</a></li>
-            <li><a href="team-member.html">Team Member</a></li>
+            <li><a href="project.html">Project</a></li>
+            <li><a href="member.html">Member</a></li>
             <li><a href="seminar.html">Seminar</a></li>
-            <li><a href="collaboration-work.html">Collaboration Works</a></li>
+            <li><a href="collaboration.html">Collaboration</a></li>
             <li><a href="research-exchange.html">Research Exchange</a></li>
             <li><a href="links.html">Links</a></li>
-            <li><a href="links.html"><img src="images/tech-logo.jpg"></a></li>
-            <li><a href="links.html"><img src="images/wrhi-logo.png"></a></li>
+            <li><a href="https://www.jsps.go.jp/english/"><img src="images/jsps-logo.png"></a></li>
+            <li><a href="https://www.titech.ac.jp/english"><img src="images/tech-logo.jpg"></a></li>
+            <li><a href="https://www.wrhi.iir.titech.ac.jp/en/"><img src="images/wrhi-logo.png"></a></li>
         </ul>
     </nav>
     <!--小さい端末用（900px以下端末）メニュー-->
@@ -25,6 +25,9 @@ const header = `
             <li><a href="collaboration-work.html">Collaboration Works</a></li>
             <li><a href="research-exchange.html">Research Exchange</a></li>
             <li><a href="links.html">Links</a></li>
+            <li><a href="https://www.jsps.go.jp/english/">JSPS</a></li>
+            <li><a href="https://www.titech.ac.jp/english">Tokyo Institute of Technology</a></li>
+            <li><a href="https://www.wrhi.iir.titech.ac.jp/en/">WRHI</a></li>
         </ul>
     </nav>
 </header>
